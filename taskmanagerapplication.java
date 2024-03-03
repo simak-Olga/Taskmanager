b6477e94bf420fe34391bfd32381e28cf738cb30
Simakhina Olga <110253758+simak-Olga@users.noreply.github.com>
2024-03-03 16:14:08 +0300

    Create taskmanagerapplication.java

---
+++ taskmanagerapplication.java
@@ -0,0 +1,13 @@
+package taskmanagerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class taskmanagerapplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(taskmanagerapplication.class, args);
+    }
+
+}
