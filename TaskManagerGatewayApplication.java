71beb1661a490d55c9b426a9c4ff019b5b623a87
Simakhina Olga <110253758+simak-Olga@users.noreply.github.com>
2024-03-03 16:25:50 +0300

    Create TaskManagerGatewayApplication.java

---
+++ TaskManagerGatewayApplication.java
@@ -0,0 +1,13 @@
+package taskmanagergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerGatewayApplication.class, args);
+    }
+
+}
